f278cdcfaa2f39de1bf7f6c3638b405e3eada981
okiki123 <okikimalomo17@gmail.com>
2022-07-01 12:05:24 +0100

    Updated Commit

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-const fs = require('fs');
 const express = require('express');
-const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.json());
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from the server side');
+})
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
 
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
 
-module.exports = app;
-
-app.use((err, req, res, next) => {
-  err.statusCode =   
-  
-  res.status();
-});
