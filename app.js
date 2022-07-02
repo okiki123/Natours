7b6697df70ebe6a364b6ea63997ef17bc779961d
okiki123 <okikimalomo17@gmail.com>
2022-07-02 12:15:07 +0100

    Updated Commit

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 app.get('/api/v1/tours', (req, res) => {
    res.status(200).json({
     status: 'success',
+    results: tours.length,
     data: {
       tours
     }
    })
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello from server');
-})
+
 
 const port = 3000;
 app.listen(port, () => {
