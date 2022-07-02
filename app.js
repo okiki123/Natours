80b2ec2a96fbe73d67a4080606254b8dadb537f7
okiki123 <okikimalomo17@gmail.com>
2022-07-02 09:52:26 +0100

    Updated Commit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side', app: 'Natours'});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('api/v1/tours', (req, res) => {
+   res.status(200).json({
+    status: 'sucess',
+    data: { tours }
+   })
 })
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint')
-})
 
 const port = 3000;
 app.listen(port, () => {
