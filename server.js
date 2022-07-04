ba54976083ab6fa4f9ed042bde4ae0c0fa7d1806
okiki123 <okikimalomo17@gmail.com>
2022-07-04 13:19:17 +0100

    Updated Commit

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env'});
-
-console.log(app.get('env'));
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
