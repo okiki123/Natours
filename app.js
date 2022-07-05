844b62a76e395c8f3d7fb9f0eb78e445f1422977
okiki123 <okikimalomo17@gmail.com>
2022-07-05 23:36:58 +0100

    Updated Commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -9,6 +10,11 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+})
+
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
